53e45711c3d43fe720733a4965d2d37fcdb50a1c
vividxz <aa5454@srmist.edu.in>
2023-07-02 18:29:22 +0530

    Stock Buy Sell, Time O(n)

--- DAY 1/StockBuySell.cpp
+++ DAY 1/StockBuySell.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int mn = INT_MAX;
-        int minPrice[prices.size()];
+        int mn = INT_MAX, diff=0, profit=0;
         for(int i=0; i<prices.size(); i++){
             mn = min(mn, prices[i]);
-            minPrice[i] = mn;
-        }
-        int diff=0, profit = 0;
-        for(int i=prices.size()-1; i>0; i--){
-            diff = prices[i]-minPrice[i];
+            diff = prices[i] - mn;
             profit = max(diff, profit);
         }
         return profit;
