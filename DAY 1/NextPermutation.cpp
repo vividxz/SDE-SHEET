7d3f3d21d7d7eebf455a884baf07636e1a330e55
vividxz <aa5454@srmist.edu.in>
2023-07-02 17:04:37 +0530

    Next Permutaion Time O(n)

---
+++ DAY 1/NextPermutation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int index = -1;
+        for(int i=nums.size()-1; i>0; i--){
+            if(nums[i] > nums[i-1]){
+                index = i-1;
+                break;
+            }         
+        }
+        if(index !=-1){
+            for(int i=nums.size()-1; i>=0; i--){
+                 if(nums[i] > nums[index]){
+                    swap(nums[i], nums[index]);
+                    break;
+                }
+            }
+        }
+        
+        reverse(nums.begin()+index+1, nums.end());
+    }
+};
