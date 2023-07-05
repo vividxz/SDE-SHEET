9a6b757186d830b612b4eed9c9a61e0e63572135
vividxz <aa5454@srmist.edu.in>
2023-07-06 01:03:27 +0530

    Two Sum O(NLogN) Space O(n)

--- DAY 4/TwoSum.cpp
+++ DAY 4/TwoSum.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>numsCopy = nums;
         vector<int> ans;
-        for(int i=0; i<nums.size();i++){
-            for(int j=i+1; j<nums.size();j++){
-                if(nums[i]+nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+        int first = 0, second = 0,i=0, j=nums.size()-1;
+        sort(numsCopy.begin(), numsCopy.end());
+        while(i<j){
+            if((numsCopy[i]+numsCopy[j]) > target)
+                j--;
+            else if((numsCopy[i]+numsCopy[j]) < target)
+                i++;
+            else{
+                first = numsCopy[i];
+                second = numsCopy[j];
+                break;
             }
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == first){
+                ans.push_back(i);
+                break;
+            }
+                
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == second && i != ans[0]){
+                ans.push_back(i);
+                break;
+            }
+                
         }
         return ans;
     }
