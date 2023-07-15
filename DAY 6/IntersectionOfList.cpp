4960022e767281a4f3df149812d9d80bfc045a37
vividxz <aa5454@srmist.edu.in>
2023-07-15 16:28:19 +0530

    Intersection Of List(Two Pointers) Time O(m+n) Space O(1)

--- DAY 6/IntersectionOfList.cpp
+++ DAY 6/IntersectionOfList.cpp
@@ -1,32 +1,11 @@
 class Solution {
 public:
-    int length(ListNode *head){
-        int l = 0;
-        while(head){
-            l++;
-            head = head->next;
-        }
-        return l;
-    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int l1 = length(headA);
-        int l2 = length(headB);
-        int diff = abs(l1-l2);
-        if(l1 > l2){
-            while(diff--)
-                headA = headA->next;
-        }else{
-            while(diff--)
-                headB = headB->next;
+        ListNode *A=headA, *B=headB;
+        while(A!=B){
+            A = A == NULL ? headB : A->next;
+            B = B == NULL ? headA : B->next;
         }
-        while(headA != headB){
-            if(headA == NULL)
-                return NULL;
-            if(headB == NULL)
-                return NULL;
-            headA = headA->next;
-            headB = headB->next;
-        }
-        return headA;
-    }   
+        return A;
+    }
 };
